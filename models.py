2f88cd140dedeb0ff19f51ed537a4fd4d8090f67
Theacollab <paul@theacollab.com>
2025-06-15 00:05:37 -0700

    Update models.py

--- models.py
+++ models.py
@@ -13,3 +13,13 @@ class Artist(db.Model):
     name = db.Column(db.String(100), nullable=False)
     genre = db.Column(db.String(100), nullable=False)
     availability = db.Column(db.String(100), nullable=False)
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+    event_date = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), default="Pending")
+
+    artist = db.relationship('Artist', backref=db.backref('bookings', lazy=True))
+    venue = db.relationship('Venue', backref=db.backref('bookings', lazy=True))
