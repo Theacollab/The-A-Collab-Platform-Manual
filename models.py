1ee24a73955e5eaefaea59ad2debaca7692bd031
Theacollab <paul@theacollab.com>
2025-06-14 23:41:31 -0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Venue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    availability = db.Column(db.String(100), nullable=False)
