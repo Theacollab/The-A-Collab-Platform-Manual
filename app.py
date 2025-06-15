1470963634445bd1a30c93ba82ec48542bb51bed
Theacollab <paul@theacollab.com>
2025-06-15 00:01:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+# Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///collab.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# Initialize Database
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
